be420a6f84dd90869b505a7037e03299f92a95b7
Sahil Dawar <sahildawarvines@gmail.com>
2025-06-21 16:58:49 +0530

    Initial commit

--- embed.py
+++
@@ -1,7 +0,0 @@
-import sys, json
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-text = sys.stdin.read()
-emb = model.encode(text).tolist()
-print(json.dumps(emb))
